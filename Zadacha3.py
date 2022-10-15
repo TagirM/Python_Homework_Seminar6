e1bc428b52fc54c1c4518a7fae3c95b0568f9c5d
Admin <tmurzagaleev@yandex.ru>
2022-10-15 12:24:50 +0700

    Решение задачи 3 из ДЗ №6

--- Zadacha3.py
+++ Zadacha3.py
@@ -1,12 +1,14 @@
 # Задача 3. Написать функцию, аргументы - имена сотрудников, возвращает словарь, ключи -
 # первые буквы имен, значения - списки, содержащие имена, начинающиеся с соответствующей буквы.
 
+from collections import defaultdict
+
 names = ["Иван", "Мария", "Петр", "Илья", "Марина", "Алина", "Бибочка", "Петр"]
 
-
 def myNewDict(arguments):
-    myDict = {j[0]: j for j in arguments}
+    myDict = defaultdict(list)    
+    for j in arguments:        
+        myDict[j[0]].append(j)    
     return myDict
-
 
 print(myNewDict(names))
